226b0c07e583a74779fc9165de9104c9ce948e15
amani esseili <esseiliamani@hotmail.com>
2023-07-29 23:57:46 +0200

    elemento html e api url

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const emailListElement = document.getElementById('emailList');  // prendo l'elemento html 
+
+const apiUrl = 'https://flynn.boolean.careers/exercises/api/random/mail'; //url dell'api per generare gli mail casuali
+
+const numberOfEmails = 10; // const con numero di mail da generare 
+
+let generatedEmails = 0;// contatore delle mail generati
+
+
+
